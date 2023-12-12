3c9bb432570ecb8cba128ac0172917197901057d
Mixie Clerard <clerardm@gmail.com>
2023-12-11 22:45:22 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// var num = document.querySelector(".num");
+// var add = document.querySelector(".add");
+
+// var a = 9;
+
+// add.addEventListener("click", ()=>{
+//     a++;
+//     num.innerText = a;
+// })
+
+// LIKE BUTTON 1
+var num = document.querySelector(".num");
+var likes = num.innerText;
+
+function btnClicks() {
+    likes++;
+    num.innerText = likes;
+}
+
+// LIKE BUTTON 2
+var num2 = document.querySelector(".num2");
+var likes2 = num2.innerText;
+
+function btnClicks2() {
+    likes2++;
+    num2.innerText = likes2;
+}
+
+// LIKE BUTTON 3
+var num3 = document.querySelector(".num3");
+var likes3 = num3.innerText;
+
+function btnClicks3() {
+    likes3++;
+    num3.innerText = likes3;
+}
